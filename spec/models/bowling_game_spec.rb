47fb7fc66c9954ae5eef0689a0bf982bb0067728
Jose <j.riquelme@and-is.de>
2015-07-04 13:43:44 +0200

    grouping methods

--- spec/models/bowling_game_spec.rb
+++ spec/models/bowling_game_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe BowlingGame, type: :model do
   
- describe '#score' do
+
  
  	subject { BowlingGame.new }
  	
- 	it { expect(subject).to respond_to(:score) }
+ 	describe "public class methods" do
+  	context "responds to its methods" do
+ 			it { expect(subject).to respond_to(:score) }
+ 		end
 
- end
+ 		context "executes methods correctly" do
+ 			it { expect(subject.score).to be 0 }
+ 		end
+
+ 	end
 
 end
+
+
