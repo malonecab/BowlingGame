9a547930ef11675e05816889e6f093e1c55b71f5
Jose <j.riquelme@and-is.de>
2015-07-04 16:13:18 +0200

    calculate score with new method to save hits

--- app/models/bowling_game.rb
+++ app/models/bowling_game.rb
@@ -2,11 +2,14 @@ class BowlingGame
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  
+  field :hits, type: Array, default: -> { Array.new() }
 
   def score
-  	0
+  	hits.inject(0) { |total, hit| total + hit }
   end
 
+  def attempt(pins)
+  	hits.push pins
+  end
 
 end
--- spec/models/bowling_game_spec.rb
+++ spec/models/bowling_game_spec.rb
@@ -8,10 +8,22 @@ RSpec.describe BowlingGame, type: :model do
  	describe "public class methods" do
   	context "responds to its methods" do
  			it { expect(subject).to respond_to(:score) }
+      it { expect(subject).to respond_to(:attempt) }
  		end
 
- 		context "executes methods correctly" do
+ 		describe "#score" do
  			it { expect(subject.score).to be 0 }
+
+      it 'should be 0 after 20 attempts with 0 pins down' do
+        20.times { subject.attempt(0) }
+        expect(subject.score).to be 0
+      end
+
+      it 'should be 20 after 20 attempts with 1 pins down' do
+        20.times { subject.attempt(1) }
+        expect(subject.score).to be 20
+      end
+
  		end
 
  	end
