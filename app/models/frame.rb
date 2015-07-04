263f1f5bbd037d5c913d0117dbfd6ce6e3f7c73b
Jose <j.riquelme@and-is.de>
2015-07-04 15:09:41 +0200

    one frame allow only 2 attemps

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -2,7 +2,19 @@ class Frame
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  MAX_TRIES = 2
   embedded_in :bowling_game
- 
+ 	
+ 	
+	field :hits, type: Array, default: -> { Array.new() }
 
+	validate :validate_hits
+	
+	def validate_hits
+    errors.add(:hits, "to many tries") if hits.size > MAX_TRIES
+  end
+
+	def hit(pins)
+		hits.push(pins)
+	end
 end
--- spec/models/frame_spec.rb
+++ spec/models/frame_spec.rb
@@ -2,12 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Frame, type: :model do
    
- 	subject { Frame.new }
+  subject { Frame.new }
 
- 	describe "associations" do
+  describe "associations" do
     it { should be_embedded_in(:bowling_game) }
   end
 
+  describe "public class methods" do
+    context "responds to its methods" do
+      it { expect(subject).to respond_to(:hit) }
+    end
+
+  end
+
+  describe "Model validations" do
+    context "successfully" do
+      it 'when only 1 hit' do
+        subject.hit(0)
+        expect(subject).to be_valid
+      end
+      it 'when only 2 hits' do
+        2.times { subject.hit(0) }
+        expect(subject).to be_valid
+      end   
+    end
+
+    context "with error" do
+      it 'when only 2 hits' do
+        3.times { subject.hit(0) }
+        expect(subject).to_not be_valid
+      end
+    end
+
+  end
 end
 
 
