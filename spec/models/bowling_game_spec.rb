8c617472b74b6bb85bb0a8e4f5e05711a8cffb89
Jose <j.riquelme@and-is.de>
2015-07-05 07:03:46 +0200

    rename tests descriptions

--- spec/models/bowling_game_spec.rb
+++ spec/models/bowling_game_spec.rb
@@ -14,23 +14,23 @@ RSpec.describe BowlingGame, type: :model do
  		describe "#score" do
  			it { expect(subject.score).to be 0 }
 
-      it 'should be 0 after 20 attempts with 0 pins down' do
+      it '0 after 20 attempts with 0 pins down' do
         20.times { subject.attempt(0) }
         expect(subject.score).to be 0
       end
 
-      it 'should be 20 after 20 attempts with 1 pins down' do
+      it '20 after 20 attempts with 1 pins down' do
         20.times { subject.attempt(1) }
         expect(subject.score).to be 20
       end
       
-      it 'should be 30 after 1 strike and 18 attempts with 1 pins down' do
+      it '30 after 1 strike and 18 attempts with 1 pins down' do
         subject.attempt(10)
         18.times { subject.attempt(1) }
         expect(subject.score).to be 30
       end
 
-      it 'should be 300 after 10 attempts with 10 pins down' do
+      it '300 after 10 attempts with 10 pins down' do
         12.times { subject.attempt(10) }
         expect(subject.score).to be 300
       end
