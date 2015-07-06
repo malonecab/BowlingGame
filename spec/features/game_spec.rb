ede1bae67506bc986ee6cd3e943ce69d2b514005
Jose <j.riquelme@and-is.de>
2015-07-06 16:25:38 +0200

    make a throw test

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -4,8 +4,8 @@ describe "GET /" do
     expect(page).to have_css("section h1", text: "Track your Bowling Scores")    
   end
 
-  context "starts game" do
-    it "starts game" do
+  context "start game" do
+    it "show actual Round and Ball" do
       visit root_path
       click_link("btn-start")
       expect(page).to have_content("Round 1")
@@ -15,6 +15,16 @@ describe "GET /" do
     it "has 10 pins available" do
       visit new_game_path
       expect(page).to have_css('div.btn_pins', count: 11)
+    end
+  end
+
+  context "throw the first ball" do
+    it "kock down 3 pins" do
+      visit new_game_path
+      click_link("btn-pins-3")
+      expect(page).to have_content("Round 1")
+      expect(page).to have_content("Ball 2")
+      expect(page).to have_css('div.btn_pins', count: 8)
     end
   end
 end
