3ab594c03eb3d015371418feba5a5461e174e52d
Jose <j.riquelme@and-is.de>
2015-07-06 11:34:06 +0200

    improve test and add new for testing start game

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,6 +1,13 @@
 describe "GET /" do
   it "display welcome page" do
     visit root_path
-    expect(page).to have_content "Track your Bowling Scores"
+    expect(page).to have_css("section h1", text: "Track your Bowling Scores")    
+  end
+
+  it "starts game" do
+  	visit root_path
+  	click_link("btn-start")
+  	expect(page).to have_content("Round 1")
+
   end
 end
