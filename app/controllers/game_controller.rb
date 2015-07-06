c4bf3d9b6cdc625c8174bafd326478d637822b0a
Jose <j.riquelme@and-is.de>
2015-07-06 12:32:47 +0200

    show round number dynamically

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,6 +4,14 @@ class GameController < ApplicationController
 	end
 
 	def show
+		round_and_ball
 	end
 
+
+	private
+
+	def round_and_ball
+		@round = params[:round_id]
+		@ball = params[:ball_id]
+	end
 end
