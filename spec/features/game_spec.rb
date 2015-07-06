33b06da444404d758931a6c1b9994c0b63fdd2ea
Jose <j.riquelme@and-is.de>
2015-07-06 14:09:58 +0200

    first, green test

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -4,10 +4,18 @@ describe "GET /" do
     expect(page).to have_css("section h1", text: "Track your Bowling Scores")    
   end
 
-  it "starts game" do
-  	visit root_path
-  	click_link("btn-start")
-  	expect(page).to have_content("Round 1")
+  context "starts game" do
+    it "starts game" do
+      visit root_path
+      click_link("btn-start")
+      expect(page).to have_content("Round 1")
+      expect(page).to have_content("Ball 1")
+    end
 
+    it "has 10 pins available" do
+      visit game_path(1,1)
+
+      expect(page).to have_css('a.btn_pins', count: 10)
+    end
   end
 end
